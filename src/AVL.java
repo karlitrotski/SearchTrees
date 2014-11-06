b60c17120648cd95e0decddb1d62fadb62c19627
ez2torta <ez2torta@gmail.com>
2014-11-06 18:00:30 -0300

    AVL Funcionando
    En la teoría

--- src/AVL.java
+++ src/AVL.java
@@ -2,22 +2,36 @@ import java.io.FileWriter;
 import java.io.PrintWriter;
 
 public class AVL {
-	Node root;
+	private NodeAVL root;
 	public AVL(){
-		this.root = new Node(-1, null);
+		this.root = null;
 	}
-	public void simpleLeftRotation(){
+	public NodeAVL simpleLeftRotation(NodeAVL k2){
+		NodeAVL k1 = k2.left;
+        k2.left = k1.right;
+        k1.right = k2;
+        k2.h = Math.max( height( k2.left ), height( k2.right ) ) + 1;
+        k1.h = Math.max( height( k1.left ), height(k2) ) + 1;
+        return k1;
+	}
+	public NodeAVL simpleRightRotation(NodeAVL k1){
+        NodeAVL k2 = k1.right;
+        k1.right = k2.left;
+        k2.left = k1;
+        k1.h = Math.max( height( k1.left ), height( k1.right ) ) + 1;
+        k2.h = Math.max( height( k2.right ), height(k1) ) + 1;
+        return k2;
+	}
+	public NodeAVL doubleLeftRotation(NodeAVL k3){
+		k3.left=simpleRightRotation(k3.left);
+		return simpleLeftRotation(k3); 
 		
 	}
-	public void simpleRightRotation(){
-		
+	public NodeAVL doubleRightRotation(NodeAVL k1){
+		k1.right = simpleLeftRotation(k1.right);
+		return simpleRightRotation(k1);
 	}
-	public void doubleLeftRotation(){
-		
-	}
-	public void doubleRightRotation(){
-		
-	}
+	
 	public Node search(int x, Node n){
 		if(n == null)
 			return null;
@@ -30,46 +44,71 @@ public class AVL {
 		else
 			return search(x, n.right);
 	}
-	public void insert(int x, Node n){
-		if(n.key == -1)
-			n.key = x;
-		else if(x < n.key){
-			if(n.left == null)
-				n.left = new Node(x, n);
-			else
-				insert(x, n.left);
+	public NodeAVL insert(int x,NodeAVL r){
+		if(r==null){
+			r = new NodeAVL(x);
+			return r;
 		}
-		else if(x > n.key){
-			if(n.right == null)
-				n.right = new Node(x, n);
-			else
-				insert(x, n.right);
+		else{
+			if(x<r.key){
+				r.left=insert(x,r.left);
+				if(height(r.right)-height(r.left)==-2){
+					if(x-r.left.key>0){
+						r=simpleLeftRotation(r);
+					}
+					else{
+						r=doubleLeftRotation(r);
+					}
+				}
+			}
+			else if(x>r.key){
+				r.right=insert(x,r.right);
+				if(height(r.left)-height(r.right)==-2){
+					if(x-r.right.key>0){
+						r=simpleRightRotation(r);
+					}
+					else{
+						r=doubleRightRotation(r);
+					}
+				}
+			}
+            r.h = Math.max( height( r.left ), height( r.right ) ) + 1;
+			return r;
 		}
-		return;
+		
 	}
-	public static void preOrder(NodeAVL r, PrintWriter pw){
-		if(r != null){
-			pw.println(r.key);
-			if(r.left != null)
-				preOrder(r.left, pw);
-			if(r.right != null)
-				preOrder(r.right, pw);
+    public void preOrder(NodeAVL n, PrintWriter pw){
+		if(n != null){
+			pw.println(n.key);
+			if(n.left != null)
+				preOrder(n.left, pw);
+			if(n.right != null)
+				preOrder(n.right, pw);
 		}
 	}
 	public static int numRandom(int min, int max){
 		return ((int)(Math.random()*max-min+1));
 	}
+	
+	public int height(NodeAVL r){
+        return r == null ? -1 : r.h;
+	}
 	static public void main(String[] args) throws Exception{
 		PrintWriter pw = new PrintWriter(new FileWriter("avl_test.txt"));
 		pw.println("AVL Test");
 		pw.println("");
-		NodeAVL r = new NodeAVL(0);
-		int N = (int)Math.pow(2, 18);
-		int max = (int)(Math.pow(2, 22)-1);
-		for(int i = 0; i < N; i++){
-			r.insertar(numRandom(1,max));
-		}
-		preOrder(r, pw);
+		AVL r = new AVL();
+//		int N = (int)Math.pow(2, 18);
+//		int max = (int)(Math.pow(2, 22)-1);
+		r.root=r.insert(0, r.root);
+		r.root=r.insert(3, r.root);
+		r.root=r.insert(4, r.root);
+		r.root=r.insert(5, r.root);
+		r.root=r.insert(6, r.root);
+//		for(int i = 0; i < N; i++){
+//			r.root=r.insert(numRandom(1,max),r.root);
+//		}
+		r.preOrder(r.root,pw);
 		//System.out.println(max);
 		pw.close();
 	}
--- src/NodeAVL.java
+++ src/NodeAVL.java
@@ -1,111 +1,37 @@
-
 public class NodeAVL {
 	int key;
 	NodeAVL left;
 	NodeAVL right;
 	int FE;
+	int h;
 	NodeAVL parent;
+	public NodeAVL(int key, NodeAVL parent){
+		this.parent=parent;
+		this.key=key;
+		this.left=null;
+		this.right=null;
+		this.FE=0;
+		this.h=0;
+	}
 	public NodeAVL(int key) {
 		this.key=key;
 		this.left=null;
 		this.right=null;
 		this.FE=0;
+		this.h=0;
 	}
-
-	public static int numRandom(int min, int max){
-		return ((int)(Math.random()*max-min+1));
-	}
-	public void insertar(int key){
-		if(this.key<key){
-			if(this.right==null){
-				NodeAVL nuevito = new NodeAVL(key);
-				nuevito.parent=this;
-				this.right=nuevito;
-				this.right.update();
-			}
-			else{
-				this.right.insertar(key);
-			}
-		}
-		else{
-			if(this.left==null){
-				NodeAVL nuevito = new NodeAVL(key);
-				nuevito.parent=this;
-				this.left=nuevito;
-				this.left.update();
-			}
-			else{
-				this.left.insertar(key);
-			}
-		}
-	}
-	public int calculateHeight(){
-		// calcular altura
-		int izq,der;
-		if(this.left==null){
-			izq=0;
-		}
-		else{
-			izq= left.FE;
-		}
-		if(this.right==null){
-			der=0;
-		}
-		else{
-			der = right.FE;
-		}
-		return der-izq;
-	}
+	
 	public void update(){
-		// actualizar FE y tomar decisiones
-		this.FE=calculateHeight();
-		if(FE < -1){
-			
+		int r=0;
+		int l=0;
+		if(this.left!=null){
+			this.left.update();
+			l=this.left.FE;
 		}
-		else if(FE > 1){
-			
+		if(this.right!=null){
+			this.right.update();
+			r=this.right.FE;
 		}
-		else{
-			
-		}
-		if(parent!=null){
-			parent.update();
-		}
-		
-		
-	}
-	public void rotarDer(){
-		NodeAVL papa = this.parent;
-		NodeAVL este = this;
-		NodeAVL hijoIzq= this.left;
-		NodeAVL hijoDer= this.right;
-		// paso 1 - nodo nuevo de la derecha
-		NodeAVL Nuevo = new NodeAVL(papa.key);
-		Nuevo.right=this.parent.right;
-		Nuevo.right.parent=Nuevo;
-		Nuevo.left=hijoDer;
-		Nuevo.left.parent=Nuevo;
-		Nuevo.parent=papa;
-		
-		// paso 2 = correr los datos a la derecha
-		
-		papa.key=this.key;
-		papa.left=this.left;
-		papa.right=Nuevo;
-		
-		this.key=hijoIzq.key;
-		this.left=hijoIzq.left;
-		this.right=hijoIzq.right;
-				
-	}
-	
-	public void rotarIzq(){
-		
-	}
-	
-	public void main(String[] args){
-		NodeAVL Arbol = new NodeAVL(0);
-		Arbol.insertar(1);
-		Arbol.insertar(2);
+		this.FE=r-l;
 	}
 }
